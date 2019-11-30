bb288adb811aec34d6e2f00c809d8a18a495b20b
Vikas Rao <mvrao@uci.edu>
2019-11-30 14:30:31 -0800

    This changelist consists of the following changes :
    1. Addition of the FOV attribute, usage of the basic (default) algorithm
    to begin with.
    2. Tile now consists of explored. This will keep track it a particular
    tile has been made visible, hence explored of not.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use tcod::colors::*;
 use tcod::console::*;
 use std::cmp;
 use rand::Rng;
-
+use tcod::map::{FovAlgorithm,Map as FovMap};
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
+
+//FOV algorithm consts
+const FOV_ALGO : FovAlgorithm = FovAlgorithm::Basic; // Default FOV algorithm
+const FOV_LIGHT_WALLS: bool = true ; // light walls or not 
+const TORCH_RADIUS: i32 = 10; 
 
 // size of the map
 const MAP_WIDTH: i32 = 80 ;
@@ -17,10 +22,20 @@ const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color {r:0, g:0, b:100};
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
 const COLOR_DARK_GROUND: Color = Color {
     r: 50,
     g: 50,
     b: 150,
+};
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
 };
 
 type Map = Vec<Vec<Tile>>;
@@ -33,6 +48,7 @@ struct Game {
 #[derive(Clone, Copy, Debug)]
 struct Tile {
     blocked: bool,
+    explored: bool,
     block_sight: bool,
 }
 
@@ -41,6 +57,7 @@ impl Tile {
     pub fn empty() -> Self {
         Tile {
             blocked: false,
+            explored: false,
             block_sight: false,
         }
     }
@@ -48,6 +65,7 @@ impl Tile {
     pub fn wall() -> Self {
         Tile {
             blocked: true,
+            explored: false,
             block_sight: true,
         }
     }
@@ -134,6 +152,7 @@ fn create_room(room: Rect, map: &mut Map) {
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 fn create_h_tunnel(x1: i32, x2: i32,y: i32,map: &mut Map){
@@ -212,18 +231,38 @@ fn make_map(player: &mut Object) -> Map {
     map
 }
 
-fn render_all(tcod: &mut Tcod, game : &Game, objects: &[Object]) {
+// main render program
+fn render_all(tcod: &mut Tcod, game : &mut Game, objects: &[Object], fov_recompute: bool) {
+    if fov_recompute { 
+        // recompute FOV if needed ( the player moved or something)
+        let player = &objects[0];
+        tcod.fov
+            .compute_fov(player.x,player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO); 
+    }
     // Traverse and set the tile color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x,y);
+
             let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con
-                        .set_char_background(x,y,COLOR_DARK_WALL,BackgroundFlag::Set);
+            let color = match (visible, wall)
+            {
+                // outside FOV:
+                (false,true) => COLOR_DARK_WALL,
+                (false,false) => COLOR_DARK_GROUND,
+                // inside FOV
+                (true,true) => COLOR_LIGHT_WALL,
+                (true,false) => COLOR_LIGHT_GROUND,
+            };
+            let explored = &mut game.map[x as usize][y as usize].explored;
+
+            if visible {
+                // as its visible, it means that it has been explored
+                *explored = true; 
             }
-            else {
-                tcod.con
-                        .set_char_background(x,y,COLOR_DARK_GROUND,BackgroundFlag::Set);
+
+            if *explored {
+                tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
@@ -245,6 +284,7 @@ fn render_all(tcod: &mut Tcod, game : &Game, objects: &[Object]) {
     );
 
 }
+// define the behaviour of the keys for control
 fn handle_keys(tcod: &mut Tcod,game: &Game, player: &mut Object ) -> bool {
     // TODO: handle keys
     use tcod::input::Key;
@@ -283,27 +323,29 @@ fn main() {
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .size(MAP_WIDTH, MAP_HEIGHT)
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    let mut tcod = Tcod { root , con };
+    let mut tcod = Tcod { 
+        root ,
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT), 
+        };
 
     // create player
     let player = Object::new(0, 0, '@', WHITE);
 
-    // create an Non-player character
-    let npc = Object::new(SCREEN_WIDTH/2 -5 , SCREEN_HEIGHT/2, '+', RED);
-
     // list with all objects in the game
-    let mut objects = [player,npc];
+    let mut objects = [player];
 
-    let game = Game {
+    let mut game = Game {
         // make the map - not rendered though
         map: make_map(&mut objects[0]),
     };
+
+    // first time FOV recomputation
+    let mut previous_player_pos = (-1,-1);
 
     while !tcod.root.window_closed() {
 
@@ -311,12 +353,14 @@ fn main() {
         tcod.con.clear();
 
         // render
-        render_all(&mut tcod, &game, &objects);
+        let fov_recompute = previous_player_pos != (objects[0].x , objects[0].y);
+        render_all(&mut tcod, &mut game, &objects,fov_recompute);
 
         tcod.root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
+        previous_player_pos = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
 
         if exit {
